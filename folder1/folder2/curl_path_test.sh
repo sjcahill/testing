f0094d8fe38a24238b67661d3538e655be89ae1c
sjcahill <sjoscahill@gmail.com>
2021-11-19 17:43:47 -0500

    help

--- folder1/folder2/curl_path_test.sh
+++ folder1/folder2/curl_path_test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
 echo "Currently executing this script from pwd: $(pwd)"
-echo "BASH_SOURCE of this script located at https://github.com/sjcahill/testing/folder1/folder2/curl_path_test.sh is ${BASH_SOURCE[0]}"
+echo "BASH_SOURCE of this script located at https://github.com/sjcahill/testing/folder1/folder2/curl_path_test.sh is:     ${BASH_SOURCE[0]}"
+BASEDIR="$(cd -- "$(dirname -- "$BASH_SOURCE[0]")" &>/dev/null && pwd)"
+echo $BASEDIR
+
