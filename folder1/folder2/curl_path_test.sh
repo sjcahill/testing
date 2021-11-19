d9aa9523ec5e76519561448317acc6d9aa721bcc
sjcahill <sjoscahill@gmail.com>
2021-11-19 17:14:17 -0500

    Initial commit

---
+++ folder1/folder2/curl_path_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+echo "Currently executing this script from pwd: $(pwd)"
+echo "BASH_SOURCE of this script located at https://github.com/sjcahill/testing/folder1/folder2/curl_path_test.sh is ${BASH_SOURCE[0]}"
